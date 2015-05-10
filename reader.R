768c6dbcc73d366173e3137334051adfccb23634
Jose Maria Zambrana Arze <contact@josezambrana.com>
2015-05-10 12:08:57 -0400

    fix function to get data

--- reader.R
+++ reader.R
@@ -1,6 +1,9 @@
 get.data <- function() {
-    if(!file.exists("exdata-data-household_power_consumption.zip")) {
-        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", method="curl")
+    if(!file.exists("data.zip")) {
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "data.zip", method="curl")
+    }
+    if(!file.exists("household_power_consumption.txt")) {
+        unzip("data.zip")
     }
 }
 
